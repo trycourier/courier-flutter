cd990e3fa4109202af806583a0a23b0bcd4dfd4e
mikemilla <mike@mikemiller.design>
2022-11-03 17:44:31 -0400

    Release script

---
+++ release.sh
@@ -0,0 +1,20 @@
+# Install Github CLI if needed
+brew install gh
+gh auth login
+
+# Install yq for parsing yml
+brew install python-yq
+
+# Get the package version from the pubspec
+PACKAGE_VERSION=$(yq .version pubspec.yaml | tr -d '"')
+echo $PACKAGE_VERSION
+
+# Add the tag
+git tag $PACKAGE_VERSION
+git push --tags
+
+# gh release create
+gh release create $PACKAGE_VERSION --generate-notes
+
+# Publish to npm
+flutter pub publish
