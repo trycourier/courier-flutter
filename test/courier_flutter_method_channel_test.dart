23398283101bba9a052226cdbf8cb7f10420e0da
mikemilla <mike@mikemiller.design>
2023-11-17 12:22:19 -0800

    Test update

--- test/courier_flutter_method_channel_test.dart
+++ test/courier_flutter_method_channel_test.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:courier_flutter/channels/courier_flutter_core_method_channel.dart';
-
-void main() {
-  CoreChannelCourierFlutter platform = CoreChannelCourierFlutter();
-  const MethodChannel channel = MethodChannel('courier_flutter');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await platform.userId(), null);
-  });
-}
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:courier_flutter/channels/courier_flutter_core_method_channel.dart';
+//
+// void main() {
+//   CoreChannelCourierFlutter platform = CoreChannelCourierFlutter();
+//   const MethodChannel channel = MethodChannel('courier_flutter');
+//
+//   TestWidgetsFlutterBinding.ensureInitialized();
+//
+//   setUp(() {
+//     channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//       return '42';
+//     });
+//   });
+//
+//   tearDown(() {
+//     channel.setMockMethodCallHandler(null);
+//   });
+//
+//   test('getPlatformVersion', () async {
+//     expect(await platform.userId(), null);
+//   });
+// }
