9945d595f49342ce7715593786f627ac827ae0be
mikemilla <mike@mikemiller.design>
2024-01-11 16:45:24 -0800

    Test

--- example/lib/pages/prefs.dart
+++ example/lib/pages/prefs.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:courier_flutter/courier_flutter.dart';
+import 'package:courier_flutter/courier_preference_channel.dart';
+import 'package:courier_flutter/courier_preference_status.dart';
 import 'package:courier_flutter/models/courier_preference_topic.dart';
 import 'package:courier_flutter/models/courier_user_preferences.dart';
+import 'package:courier_flutter_sample/main.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -24,30 +28,6 @@ class _PrefsPageState extends State<PrefsPage> {
   void initState() {
     super.initState();
     _start();
-  }
-
-  // To update a topic
-  // await Courier.shared.putUserPreferencesTopic(
-  //     topicId: 'YOUR_ID',
-  //     status: CourierUserPreferencesStatus.optedIn,
-  //     hasCustomRouting: true,
-  //     customRouting: [CourierUserPreferencesChannel.push]
-  // );
-
-  _showAlert(BuildContext context, String title, String body) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(title),
-        content: Text(body),
-        actions: [
-          TextButton(
-            child: const Text('Ok'),
-            onPressed: () => Navigator.pop(context),
-          )
-        ],
-      ),
-    );
   }
 
   Future<void> _start() async {
@@ -75,7 +55,60 @@ class _PrefsPageState extends State<PrefsPage> {
     return _start();
   }
 
-  Widget _buildContent() {
+  List<CourierUserPreferencesChannel> _getRandomChannels() {
+    List<CourierUserPreferencesChannel> channelValues = [
+      CourierUserPreferencesChannel.directMessage,
+      CourierUserPreferencesChannel.email,
+      CourierUserPreferencesChannel.push,
+      CourierUserPreferencesChannel.sms,
+      CourierUserPreferencesChannel.webhook,
+    ];
+
+    Random random = Random();
+    int randomCount = random.nextInt(channelValues.length + 1);
+    List<CourierUserPreferencesChannel> randomChannels = [];
+
+    while (randomChannels.length < randomCount) {
+      CourierUserPreferencesChannel randomChannel = channelValues[random.nextInt(channelValues.length)];
+      if (!randomChannels.contains(randomChannel)) {
+        randomChannels.add(randomChannel);
+      }
+    }
+
+    return randomChannels;
+  }
+
+  Future<void> _updateTopic(BuildContext context, String topicId) async {
+    try {
+
+      setState(() {
+        _isLoading = true;
+      });
+
+      final topic = await Courier.shared.getUserPreferencesTopic(topicId: topicId);
+
+      await Courier.shared.putUserPreferencesTopic(
+        topicId: topic.topicId,
+        status: CourierUserPreferencesStatus.optedIn,
+        hasCustomRouting: true,
+        customRouting: _getRandomChannels(),
+      );
+
+      setState(() {
+        _isLoading = false;
+      });
+
+    } catch (e) {
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      rethrow;
+    }
+  }
+
+  Widget _buildContent(BuildContext buildContext) {
     if (_isLoading) {
       return const Center(
         child: CircularProgressIndicator(),
@@ -97,9 +130,10 @@ class _PrefsPageState extends State<PrefsPage> {
           itemBuilder: (BuildContext context, int index) {
             final topic = _preferences!.items[index];
             return InkWell(
-              onTap: () async {
-                final prefTopic = await Courier.shared.getUserPreferencesTopic(topicId: topic.topicId);
-                _showAlert(context, prefTopic.topicId, prefTopic.toJson());
+              onTap: () {
+                _updateTopic(context, topic.topicId).catchError((error) {
+                  showAlert(buildContext, "Error Updating Preferences", error.toString());
+                });
               },
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
@@ -121,7 +155,7 @@ class _PrefsPageState extends State<PrefsPage> {
       appBar: AppBar(
         title: const Text('Preferences'),
       ),
-      body: _buildContent(),
+      body: _buildContent(context),
     );
   }
 }
