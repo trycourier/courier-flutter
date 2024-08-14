e26b05c1f1f3c7f583cd3dbf35abbadce0908eaf
mikemilla <mike@mikemiller.design>
2024-08-14 12:38:20 -0500

    🚀 Bump version to 3.0.2

--- scripts/build_demo_app.sh
+++ scripts/build_demo_app.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+# Function to increment build number
+increment_build_number() {
+    local pubspec_file="pubspec.yaml"
+    if [ ! -f "$pubspec_file" ]; then
+        echo "❌ $pubspec_file not found."
+        exit 1
+    fi
+
+    local build_number=$(grep -E '^\s*version:' "$pubspec_file" | sed -E 's/.*\+([0-9]+).*/\1/')
+    if [ -z "$build_number" ]; then
+        echo "❌ Build number not found in $pubspec_file."
+        exit 1
+    fi
+
+    build_number=$((build_number + 1))
+    echo "Incrementing build number to $build_number."
+
+    sed -i '' -E "s/(version: .+)\+[0-9]+/\1+$build_number/" "$pubspec_file"
+}
+
 # Navigate to the example directory
 cd ../example || { echo "Failed to navigate to the example directory. Please ensure the path is correct."; exit 1; }
 echo "Navigated to the example directory."
+
+# Increment Flutter build number
+increment_build_number
 
 # Build Android app bundle
 echo "🤖 Building Android app bundle..."
